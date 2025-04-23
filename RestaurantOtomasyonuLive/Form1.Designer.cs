48465e7b28a123456897d4b153adb55aeed81914
umut <umutergen6200@gmail.com>
2025-04-23 18:07:13 +0300

    Add project files.

---
+++ RestaurantOtomasyonuLive/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace RestaurantOtomasyonuLive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.txt_mail = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.lbl_userName = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Crimson;
+            this.panel1.Controls.Add(this.txt_password);
+            this.panel1.Controls.Add(this.txt_mail);
+            this.panel1.Controls.Add(this.btn_login);
+            this.panel1.Controls.Add(this.lbl_password);
+            this.panel1.Controls.Add(this.lbl_userName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 461);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(154, 155);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(182, 22);
+            this.txt_password.TabIndex = 5;
+            // 
+            // txt_mail
+            // 
+            this.txt_mail.Location = new System.Drawing.Point(154, 117);
+            this.txt_mail.Name = "txt_mail";
+            this.txt_mail.Size = new System.Drawing.Size(182, 22);
+            this.txt_mail.TabIndex = 5;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(261, 205);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "Giriş Yap";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Location = new System.Drawing.Point(29, 155);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(34, 16);
+            this.lbl_password.TabIndex = 3;
+            this.lbl_password.Text = "Şifre";
+            // 
+            // lbl_userName
+            // 
+            this.lbl_userName.AutoSize = true;
+            this.lbl_userName.Location = new System.Drawing.Point(29, 117);
+            this.lbl_userName.Name = "lbl_userName";
+            this.lbl_userName.Size = new System.Drawing.Size(79, 16);
+            this.lbl_userName.TabIndex = 3;
+            this.lbl_userName.Text = "Kullanıcı Adı";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(350, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(495, 461);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 461);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.Label lbl_userName;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.TextBox txt_mail;
+        private System.Windows.Forms.TextBox txt_password;
+    }
+}
+
---
+++ RestaurantOtomasyonuLive/sqlMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOtomasyonuLive
+{
+    internal class sqlMethods
+    {
+        public static bool login(string mail, string psswrd)
+        {
+            Connection connection = new Connection();
+            SqlCommand cmd = new SqlCommand("giris_proc2", connection.Connect);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            SqlParameter p_mail = new SqlParameter("mail", mail); // databasede oluşturduğumuz procedure'daki parametre adı / bu metoda parametre olarak gelen kadi değeri
+            SqlParameter p_psswrd = new SqlParameter("psswrd", psswrd);
+            cmd.Parameters.Add(p_mail);
+            cmd.Parameters.Add(p_psswrd);
+            SqlDataReader read = cmd.ExecuteReader();
+            if (read.Read())
+                return true;
+            else
+                return false;
+        }
+    }
+}
